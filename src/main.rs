c04102fd42081d07e3d76bd02f329dcdcc646579
Martin Saposnic <martinsaposnic@gmail.com>
2024-12-28 16:42:56 -0300

    Add tests, serde, inputs and outputs

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,34 @@
 use std::io::Read;
+use serde::Serialize;
 
+#[derive(Debug, Serialize)]
+struct Transaction{
+    version: u32,
+    inputs: Vec<Input>,
+    outputs: Vec<Output>
+}
+
+#[derive(Debug, Serialize)]
+struct Input {
+    txid: String,
+    output_index: u32,
+    script_sig: String,
+    sequence: u32
+}
+
+struct Amount(u64);
+
+impl Amount {
+    fn to_btc(&self) -> f64 {
+        self.0 as f64 / 100_000_000.0
+    }
+}
+
+#[derive(Debug, Serialize)]
+struct Output {
+    amount: f64,
+    script_pubkey: String
+}
 fn read_compact_size(transaction_bytes: &mut &[u8]) -> u64 {
     let mut compact_size = [0; 1];
     transaction_bytes.read(&mut compact_size).unwrap();
@@ -24,20 +53,71 @@ fn read_compact_size(transaction_bytes: &mut &[u8]) -> u64 {
     }
 }
 
-fn read_version(bytes_slice: &mut &[u8]) -> u32 {
+fn read_u32(bytes_slice: &mut &[u8]) -> u32 {
     let mut buffer = [0; 4];
     bytes_slice.read(&mut buffer).unwrap();
     u32::from_le_bytes(buffer)
+}
+
+fn read_amount(bytes_slice: &mut &[u8]) -> Amount {
+    let mut buffer = [0; 8];
+    bytes_slice.read(&mut buffer).unwrap();
+    Amount(u64::from_le_bytes(buffer))
+}
+
+fn read_txid(bytes_slice: &mut &[u8]) -> String {
+    let mut txid = [0; 32];
+    bytes_slice.read(&mut txid).unwrap();
+    txid.reverse();
+    hex::encode(txid)
+}
+
+fn read_script(bytes_slice: &mut &[u8]) -> String {
+    let script_size = read_compact_size(bytes_slice);
+    let mut script = vec![0_u8; script_size as usize];
+    bytes_slice.read(&mut script).unwrap();
+    hex::encode(script)
 }
 
 fn main() {
     let _transaction_hex = "010000000242d5c1d6f7308bbe95c0f6e1301dd73a8da77d2155b0773bc297ac47f9cd7380010000006a4730440220771361aae55e84496b9e7b06e0a53dd122a1425f85840af7a52b20fa329816070220221dd92132e82ef9c133cb1a106b64893892a11acf2cfa1adb7698dcdc02f01b0121030077be25dc482e7f4abad60115416881fe4ef98af33c924cd8b20ca4e57e8bd5feffffff75c87cc5f3150eefc1c04c0246e7e0b370e64b17d6226c44b333a6f4ca14b49c000000006b483045022100e0d85fece671d367c8d442a96230954cdda4b9cf95e9edc763616d05d93e944302202330d520408d909575c5f6976cc405b3042673b601f4f2140b2e4d447e671c47012103c43afccd37aae7107f5a43f5b7b223d034e7583b77c8cd1084d86895a7341abffeffffff02ebb10f00000000001976a9144ef88a0b04e3ad6d1888da4be260d6735e0d308488ac508c1e000000000017a91476c0c8f2fc403c5edaea365f6a284317b9cdf7258700000000";
     let transaction_bytes = hex::decode(_transaction_hex).unwrap();
     let mut bytes_slice = transaction_bytes.as_slice();
-    let _version = read_version(&mut bytes_slice);
-    let input_length = read_compact_size(&mut bytes_slice);
-    println!("Version: {_version}");
-    println!("Input length: {input_length}");
+    let _version = read_u32(&mut bytes_slice);
+    let input_count = read_compact_size(&mut bytes_slice);
+    let mut inputs = vec![];
+    for _ in 0..input_count {
+        let txid = read_txid(&mut bytes_slice);
+        let output_index = read_u32(&mut bytes_slice);
+        let script_sig = read_script(&mut bytes_slice);
+        let sequence = read_u32(&mut bytes_slice);
+
+        inputs.push(Input {
+            txid,
+            output_index,
+            script_sig,
+            sequence
+        });
+    }
+
+    let output_count = read_compact_size(&mut bytes_slice);
+    let mut outputs = vec![];
+    for _ in 0..output_count {
+        let amount = read_amount(&mut bytes_slice).to_btc();
+        let script_pubkey = read_script(&mut bytes_slice);
+
+        outputs.push(Output {
+            amount,
+            script_pubkey
+        });
+    }
+
+    let transaction = Transaction {
+        version: _version,
+        inputs,
+        outputs
+    };
+    println!("Transaction: {}", serde_json::to_string_pretty(&transaction).unwrap());
 }
 
 #[cfg(test)]
